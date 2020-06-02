63793b96015a2fb098fb48e5f74bdc0c029689dc
Mario Alonso <mario.blazter99@gmail.com>
2020-06-02 02:00:29 -0500

    Part-3, Quinto Commit, TDD, Creando Model Manager

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
-from django.conf import settings
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
+from django.conf import settings
+
 
 urlpatterns = [
     path ('', include('core.urls')),
@@ -26,9 +28,10 @@ urlpatterns = [
     #Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
-
     # Paths de profiles
     path('profiles/', include(profiles_patterns)),
+     # Paths de Messenger
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
